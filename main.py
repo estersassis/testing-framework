2b667e76bfc0be27ddbef8bc9761821acef320ba
Ester Sara Assis Silva <ester.assis@prosperita.io>
2025-09-28 12:24:00 -0300

    7. Executando Todos os Testes

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from test_runner import TestRunner
 from test_loader_test import TestLoaderTest
 from test_loader import TestLoader
+from test_suite_test import TestSuiteTest
+from test_case_test import TestCaseTest
+from test_suite import TestSuite
 
 
 loader = TestLoader()
-suite = loader.make_suite(TestLoaderTest)
+test_case_suite = loader.make_suite(TestCaseTest)
+test_suite_suite = loader.make_suite(TestSuiteTest)
+test_load_suite = loader.make_suite(TestLoaderTest)
+
+suite = TestSuite()
+suite.add_test(test_case_suite)
+suite.add_test(test_suite_suite)
+suite.add_test(test_load_suite)
 
 runner = TestRunner()
 runner.run(suite)
